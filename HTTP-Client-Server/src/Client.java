5463ffdd5df842b5e4a5a28506f4c70d4fac1e6a
Gebruiker <Gebruiker@10.46.131.168>
2017-03-10 12:49:22 +0100

    client

--- HTTP-Client-Server/src/Client.java
+++ HTTP-Client-Server/src/Client.java
@@ -1,4 +1,23 @@
+import java.net.Socket;
 
+/**
+ * 
+ * TODO
+ * uitzoeken hoe java executable wordt opgeroepen in terminal
+ * de Reader haalt het HTTPCommand eruit: (GET HEAD PUT POST)
+ * URI en Port (onze server heeft port 80)
+ * 
+ * request wordt behandeld:
+ * toon response op terminal EN sla op in html bestand
+ * Als er images in het html bestand staan: gebruik GET om ze terug te geven
+ * 
+ * voor PUT en POST geef een nieuwe input lijn
+ * 
+ *
+ */
 public class Client {
-
+	public static void main(String ars[]){
+		Socket socket = new Socket();
+		System.out.println(socket.getPort());
+	}
 }
